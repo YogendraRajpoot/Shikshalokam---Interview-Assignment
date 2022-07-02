6430f9db8c03d649729ca9301f9e63b302f466f3
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-07-02 23:21:02 +0530

    Assignment 2 completed

--- Assignment2/Index.js
+++ Assignment2/Index.js
@@ -1,7 +1,11 @@
-// Given a string s, find the ASCII value of each character iteratively. If the ASCII value is even, increment the next character by (ASCII_value % 7). If the ascii value is odd, decrement the previous character by (ASCII_value % 5). Output the newly formed string. 
+// Given a string s, find the ASCII value of each character iteratively.
+// If the ASCII value is even, increment the next character by
+// (ASCII_value % 7). If the ascii value is odd, decrement the previous
+// character by (ASCII_value % 5). Output the newly formed string.
 // Note:
-// If a character has already been changed once, do not change that character again. 
-// If the new number is an invalid ASCII value, replace it with 83. 
+// If a character has already been changed once, do not change that character
+// again.
+// If the new number is an invalid ASCII value, replace it with 83.
 // Eg.
 
 // s: sHQen}
@@ -20,5 +24,40 @@
 // Sixth pass(83):
 // 	115-69-83-101-107-83
 
-// Final Answer: 
+// Final Answer:
 // 	115-69-83-101-107-83 => sESekS
+
+let string = "sHQen}";
+
+string = string.split("");
+let arr = string;
+// let arr = Array(string.length).fill(0);
+
+function newString(string) {
+  for (let i = 0; i < string.length; i++) {
+    if (
+      (string[i].charCodeAt(0) > 64 && string[i].charCodeAt(0) < 91) ||
+      (string[i].charCodeAt(0) > 96 && string[i].charCodeAt(0) < 123)
+    ) {
+      if (string[i].charCodeAt(0) % 2 === 0) {
+        if (arr[i + 1] === string[i + 1] && i + 1 < string.length) {
+          arr[i + 1] = String.fromCharCode(
+            string[i + 1].charCodeAt(0) + (string[i].charCodeAt(0) % 7)
+          );
+          arr[i + 1] = string[i + 1];
+        }
+      } else {
+        if (arr[i - 1] === string[i - 1] && i - 1 >= 0) {
+          arr[i - 1] = String.fromCharCode(
+            string[i - 1].charCodeAt(0) - (string[i].charCodeAt(0) % 5)
+          );
+          arr[i - 1] = string[i - 1];
+        }
+      }
+    } else {
+      arr[i] = "S";
+    }
+  }
+  console.log(arr.join(""));
+}
+newString(string);
